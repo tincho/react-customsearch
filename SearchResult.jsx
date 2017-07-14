83bfabbeb5c28e66c414ef30f29bd5ffd5dafa41
Martin Salinas <msalinas@quadiontech.com>
2017-07-14 11:21:26 -0300

    whole lot of cleanup, refactor and cosmetics in SearchResult

--- SearchResult.jsx
+++ SearchResult.jsx
@@ -3,56 +3,94 @@ import SearchResultRow from './SearchResultRow.jsx';
 import Pagination from './Pagination.jsx';
 
 const get = (haystack, needle, spoon) => haystack[needle] || spoon;
-const invertDirection = d => get({ ASC: "DESC", DESC: "ASC" }, d, "ASC");
+const invertDirection = d => get({
+  ASC: "DESC",
+  DESC: "ASC"
+}, d, "ASC");
 
-function ColumnHeader(props) {
-    let order = props.currentOrder;
-    let onClick = evt => {
-        evt.preventDefault();
-        order.field = props.colName;
-        if (order.field === props.colName) {
-            order.direction = invertDirection(order.direction.trim());
-        }
-        props.onChangeOrder(order.field, order.direction);
+function SortHeader(props) {
+  let {field, direction, colName} = props,
+    className = (field === colName)
+      ? "orderField orderField--" + direction
+      : "",
+    onChangeOrder = props.onChangeOrder,
+    onClick = evt => {
+      evt.preventDefault();
+      if (field === colName) {
+        direction = invertDirection(direction.trim());
+      }
+      onChangeOrder(colName, direction);
     };
-
-    let className = (order.field === props.colName) ? "orderField " + "orderField--" + order.direction : "";
-
-    return (props.enableOrder) ? <a href="#" onClick={onClick} className={className}>{props.colName}</a> : <span>{props.colName}</span>;
+  return <a href="#" className={className} onClick={onClick}>{colName}</a>;
 };
 
-export default class SearchResult extends Component {
+function ColumnHeader(props) {
+  let content = (props.resultsCount > 0)
+    ? <SortHeader {...props.order} colName={props.colName}/>
+    : props.colName;
+  return (
+    <th style={{ textTransform: 'capitalize' }}>
+      {content}
+    </th>
+  );
+}
 
-    render() {
-        let {cols, rows} = this.props;
-        let {limit = 20, offset = 0, total, onNext, onPrev, onChangeOrder, orderField, orderDirection } = this.props;
-        let pagination = { onPrev, onNext, limit, offset, total, count: rows.length || 0 };
-        let currentOrder = { field: orderField, direction: orderDirection };
+export default function SearchResult(props) {
+  let {
+    cols,
+    rows,
 
-        let data_ths   = cols.map(
-            (col, i) =>  <th key={i} style={{ textTransform: 'capitalize' }}>
-                <ColumnHeader enableOrder={rows.length > 0} currentOrder={currentOrder} onChangeOrder={onChangeOrder} colName={col} />
-            </th>);
-        let action_ths = '';// actions.map((action,i) => <th key={i}>{action}</th>); ...in the future
-        // let header_row = <tr>{data_ths}{action_ths}</tr>;
-        let header_row = <tr>{data_ths}</tr>;
+    limit = 20,
+    offset = 0,
+    total,
 
-        let noResultsText = 'Sin resultados',
-            noResultsRow  = (<tr><td colSpan={cols.length} className="text-center">
-                    <span className="text-muted">{noResultsText}</span>
-                </td></tr>);
+    onNext,
+    onPrev,
+    onChangeOrder,
 
-        let data_rows = rows.length
-            ? rows.map((row, i) => <SearchResultRow row={i} key={i} cols={row} displayCols={cols} />)
-            : noResultsRow;
-        return (
-            <div>
-                <Pagination {...pagination} />
-                <table className="table table-stripped table-bordered table-hover">
-                    <thead>{header_row}</thead>
-                    <tbody>{data_rows}</tbody>
-                </table>
-            </div>
-        );
-    }
+    orderField,
+    orderDirection
+  } = props;
+
+  let pagination = {
+    onPrev,
+    onNext,
+    limit,
+    offset,
+    total,
+    count: rows.length || 0
+  };
+  let order = {
+    field: orderField,
+    direction: orderDirection,
+    onChangeOrder
+  };
+
+  let data_ths = cols.map((col, i) => <ColumnHeader key={i} colName={col} order={order} resultsCount={rows.length}/>);
+
+  // @TODO let action_ths = actions.map((action,i) => <th key={i}>{action}</th>); then render after data_ths
+
+  let noResultsText = 'Sin resultados',
+    noResultsRow = (
+      <tr>
+        <td colSpan={cols.length} className="text-center">
+          <span className="text-muted">{noResultsText}</span>
+        </td>
+      </tr>
+    );
+
+  let data_rows = rows.length
+    ? rows.map((row, i) => <SearchResultRow row={i} key={i} cols={row} displayCols={cols}/>)
+    : noResultsRow;
+  return (
+    <div>
+      <Pagination {...pagination}/>
+      <table className="table table-stripped table-bordered table-hover">
+        <thead>
+          <tr>{data_ths}</tr>
+        </thead>
+        <tbody>{data_rows}</tbody>
+      </table>
+    </div>
+  );
 }
