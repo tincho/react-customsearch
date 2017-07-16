585615aa0c007f169be4940fe1bdae7d5adbdd56
tincho <martin.salinas@gmail.com>
2017-07-16 18:36:51 -0300

    refactoring pagination: @TODO make window of max visible page number links

--- Pagination.jsx
+++ Pagination.jsx
@@ -1,68 +1,50 @@
 import React from 'react';
 
-// @TODO get rid of Prev and Next, use only PaginationLink (figure out the prev and next page instead of prev and next offset)
-
-function Prev({total, count,  offset = 0, onPaginate, limit}) {
-    offset = parseInt(offset, 10);
-    limit = parseInt(limit, 10);
-    let btn = <a className="disabled">&lsaquo;</a>;
-    if (offset >= limit) {
-        let onClick = (ev) => {
-            ev.preventDefault();
-            onPaginate(offset - limit);
-        }
-        btn = <a href="#" onClick={onClick}>&lsaquo;</a>;
-    }
-    return <li>{btn}</li>;
-}
-
-function Next({total, count, offset = 0, onPaginate, limit}) {
-    offset = parseInt(offset, 10);
-    limit = parseInt(limit, 10);
-    let btn = <a className="disabled">&rsaquo;</a>;
-    if (total > count + offset) {
-        let onClick = (ev) => {
-            ev.preventDefault();
-            onPaginate(offset + limit);
-        }
-        btn = <a href="#" onClick={onClick}>&rsaquo;</a>;
-    }
-    return <li>{btn}</li>;
-}
+const firstAndLastPage = ({currentPage, max, pages}) => {
+    let result = [];
+    //result = [1, pages];
+    return result;
+};
+const getOffset = (lim, p) => lim * Math.max(p - 1, 0);
+const getPage = (lim, o) => 1 + Math.ceil(o / lim);
 
 export default function Pagination({offset, count, total, limit, onPaginate})  {
-    //let page = 1;
     offset = parseInt(offset, 10);
-    let resume = count > 0
-        ? <small className="text-muted">{(offset + 1) + ' - ' + (offset + count) + ' / ' + total}</small>
-        : '';
 
-    const pageOffset = (lim, p) => lim * Math.max(p - 1, 0);
-
-    const PaginationLink = ({page, text, onPaginate}) => {
-        let newOffset = pageOffset(limit, page),
+    // @TODO extract PaginationLink and make one ad-hoc partially applying to it limit and offset
+    // this way we're getting rid of the closure coupling
+    const PaginationLink = ({page, text, onClick}) => {
+        let newOffset = getOffset(limit, page),
             isCurrent = !text && count > 0 && newOffset === offset;
         return <li className={isCurrent && 'active'}>
-            <a onClick={ () => onPaginate(newOffset) } className={isCurrent && 'disabled'} href="#">{text || page}</a>
+            <a onClick={ () => onClick(newOffset) } className={isCurrent && 'disabled'} href="#">{text || page}</a>
         </li>;
     }
 
-    let pageButtons = [];
-    let pages = Math.floor(total / limit) + 1;
-    for (let page = 1; page <= pages; page++) {
-        pageButtons.push(<PaginationLink key={page} onPaginate={onPaginate} page={page} />);
-    }
+    let pages = Math.floor(total / limit) + 1,
+        currentPage = getPage(limit, offset),
+        prevPage = Math.max(1, currentPage - 1),
+        nextPage = Math.min(pages, currentPage + 1);
+    let [firstPage = 1, lastPage = pages] = firstAndLastPage({pages, currentPage, max: 5});
 
-    let props = {offset, count, total, limit, onPaginate};
+    let pageButtons = [];
+    for (let page = firstPage; page <= lastPage; page++) {
+        let btnProps = { page, onClick: onPaginate, key: page };
+        pageButtons.push(<PaginationLink {...btnProps} />);
+    }
 
     let controls = count > 0
         ?         <ul className="pagination">
-                    <PaginationLink text="&laquo;" onPaginate={onPaginate} page="1" />
-                    <Prev {...props} />
+                    <PaginationLink text="&laquo;" onClick={onPaginate} page="1" />
+                    <PaginationLink text="&lsaquo;" onClick={onPaginate} page={prevPage} />
                     {pageButtons}
-                    <Next {...props} />
-                    <PaginationLink text="&raquo;" onPaginate={onPaginate} page={pages} />
+                    <PaginationLink text="&rsaquo;" onClick={onPaginate} page={nextPage} />
+                    <PaginationLink text="&raquo;" onClick={onPaginate} page={pages} />
                 </ul>
+        : '';
+
+    let resume = count > 0
+        ? <small className="text-muted">{(offset + 1) + ' - ' + (offset + count) + ' / ' + total}</small>
         : '';
 
     return <div>
